e417d0651957af7fec399e6756384b2b597e57c5
Lou Kosak <lou.kosak@airbnb.com>
2014-06-26 00:25:59 -0700

    make grime on demand

--- grime.rb
+++ grime.rb
@@ -25,34 +25,22 @@ end
 class Grime
   BASE_URL = "http://grime.herokuapp.com"
 
-  class Mailer
-    RECIPIENTS = %W(lkosak@gmail.com tfsapinski@gmail.com)
-
-    def self.new_box_email(box_month)
-      # Hack to deal with symbol vs. string keys in the data hash
-      box_month.reload
-
-      url = "#{BASE_URL}/date/#{box_month.date}"
-      month = box_month.data['month']
-      html_body = ERB.new(File.read('views/new_box_email.erb')).result(binding)
-
-      Pony.mail(to: RECIPIENTS,
-                subject: 'New UBL report available',
-                html_body: html_body)
-    end
-  end
-
   class Fetcher
     def call
       client = Clients::USSquash.new(ENV['USS_USERNAME'], ENV['USS_PASSWORD'])
       box_id = client.current_box_id
       data = client.box_data(box_id)
-      box_month = BoxMonth.create!({ date: Date.today, data: data })
-      Grime::Mailer.new_box_email(box_month)
+      BoxMonth.create!({ date: Date.today, data: data })
     end
   end
 
   class Web < Sinatra::Application
+    get '/' do
+      box_month = BoxMonth.where(date: Date.today).last
+      box_month ||= Grime::Fetcher.call
+      erb :box_month, locals: { date: box_month.date, data: box_month.data }
+    end
+
     get '/date/:date' do |d|
       date = Date.strptime(d, "%Y-%m-%d")
       box_month = BoxMonth.where(date: date).last
